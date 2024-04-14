f7172fb385eb64fdff5aa62556f0dc79c642099f
Chris Pickel <sfiera@twotaled.com>
2024-04-14 12:15:25 +0900

    Add tech/index.rst

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,12 +8,6 @@ THEME = 'basic'
 
 TIMEZONE = 'Asia/Tokyo'
 
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 PAGE_PATHS        = [""]
 PAGE_URL          = "{slug}"
 PAGE_SAVE_AS      = PAGE_URL + ".html"
@@ -25,24 +19,20 @@ STATIC_SAVE_AS    = "{path}"
 DIRECT_TEMPLATES = []
 ARTICLE_PATHS     = []
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = False
 DISPLAY_PAGES_ON_MENU = False
 MENUITEMS = [
     ("Files",       "/file"),
     ("Cartridges",  "/cart"),
+    ("Technology",  "/tech"),
     ("GBKiss Link", "/link"),
 ]
 
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+LINKS = []
+SOCIAL = []
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
